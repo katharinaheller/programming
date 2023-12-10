05ff3799499126b6f1da544a0936ce6037ff6979
Katharina Heller <katharina.heller@4n.de>
2023-12-10 10:40:21 +0100

    new commit

---
+++ .daily_programming/datatypes_in_python.py
@@ -0,0 +1,34 @@
+# day 6
+# datatypes in python
+
+# Integer (Ganzzahl)
+integer_variable = 22
+# Ausgabe vom Wert mit dem Datentyp "Integer":
+print(integer_variable) # print-Befehl bei allen Datentypen mit der selben Syntax
+
+# Float (Gleitkommazahl)
+float_variable = 3.14 
+
+# String (Zeichenkette)
+string_variable = "Hallo, Welt!!!"
+
+#Boolean (Wahrheitswert)
+boolean_variable = True
+
+# list (Liste)
+list_variable = [1,2,3,4,5,6]
+
+# tuple (Tupel)
+tuple_variable = (10,20,30)
+print(tuple_variable)
+
+# Dictionary (Wörterbuch) 
+# Das Dictonarie in Python enthält Schlüssel und die dazugehörigen Werte, die durch ein Doppelpunkt getrennt werden. Die daraus resultierenden Paare werden durch Kommas getrennt.
+dictionary_variable = {"Name": "Katharina", "Alter": 18, "Stadt": "Mosbach"} # oft ist es nicht erforderlich, bei einfachen numerischen Werten Gänsefüßchen zu setzen (siehe Ziffer 18 im Dictonarie)
+print(dictionary_variable)
+
+# set (Menge)
+# Ein Set ist eine ungeordnete Sammlung von eindeutigen Elementen. --> Jedes Element in einem Set ist einzigartig und die Reihenfolge der Elemnte spielt keine Rolle
+set_variable = {1,2,3,4,5,6}
+
+
